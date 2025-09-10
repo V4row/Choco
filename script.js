07023576b6bc40e04d2c7ea9e0c5dbf470632771
abel <giganigab09@gmail.com>
2025-09-10 17:02:22 +0700

    sybau

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let username;
 username = window.prompt("anata no namae wa nandesuka?:");
+window.alert("Sosok hitam telah muncul")
 document.querySelector("h1").innerText = `Konichiwa ${username}-kun!, CHOCO e yokoso!`;
+
 
 
 
