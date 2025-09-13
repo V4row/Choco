f841c3990b596d15d8980f00cf17a2f00bb3488e
abel <giganigab09@gmail.com>
2025-09-13 20:05:31 +0700

    aa

---
+++ test.py
@@ -0,0 +1,23 @@
+nama =  input('nama kamu siapa? ')
+umur = input('umur kamu berapa? ')
+kode = input('masukan kode anda ')
+
+punya_duit= True
+
+if nama == 'abel' or 'vebyan' or 'farel':
+    print('hallo ' + nama + ' anda diterima')
+else:
+    print('kamu siapa?')
+if umur == 18 or 17 or 19:
+    print('umur anda kami kenalabel')
+else:
+    print('anda tidak kami kenal')
+if kode == 1208:
+    print('silahkan masuk oni-channn~~, aku basahhh~~~~')
+else:
+    print('aaahhh~~ ahhh~~ s-salah m-masuk~~~ hngggghhhh~~ >///<')
+    
+
+    
+
+
